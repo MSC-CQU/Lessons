49e6279ae5219b7036f0d08af0e261e760ca9027
SummerRegicide <starcraft17@126.com>
2016-11-05 22:15:50 +0800

    Add files via upload

---
+++ Lesson_1/Class_demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lesson
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Student Chairman = new Student("Dage", 3, 59.9, (Gender)11);
+
+			Chairman.Sex = Gender.男;
+			Chairman.StrName = "Dajie";
+
+			Console.WriteLine("{0}'s sex is {1}, age is {2}, got {3}", Chairman.StrName, Chairman.Sex, Chairman.m_iAge, Chairman.m_dScore);
+
+			Chairman.Study();
+
+			double chmScore = 60;
+			double truth = Chairman.GuessScore(chmScore);
+			Console.WriteLine("chm's score is {0}", truth);
+
+		}
+	}
+}
---
+++ Lesson_1/Class_demo/Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lesson
+{
+	class Student
+	{
+		private string m_strName;	//字段，名字
+		public int m_iAge;
+		public double m_dScore;
+		//private string m_strSex;
+		private Gender m_sex;
+
+		public string StrName		//属性
+		{
+			get
+			{
+				return m_strName;
+			}
+
+			set
+			{
+				if (value == "Dajie")
+				{
+					m_strName = "CHM";
+					return;
+				}
+				m_strName = value;
+			}
+		}
+
+		internal Gender Sex
+		{
+			get
+			{
+				return m_sex;
+			}
+
+			set
+			{
+				m_sex = value;
+			}
+		}
+
+		public Student(string name, int age, double score, Gender sex)
+		{
+			StrName = name;
+			m_iAge = age;
+			m_dScore = score;
+			Sex = sex;
+		}
+
+		public void Study()	//method = operate = function
+		{
+			Console.WriteLine("I'm {0}, I like studying", StrName);
+		}
+
+		public double GuessScore(double score)
+		{
+			if (score.Equals(m_dScore))
+			{
+				Console.WriteLine("You're right");
+			} 
+			else
+			{
+				Console.WriteLine("Wrong! {0}", m_dScore);
+			}
+			return m_dScore;
+		}
+	} 
+}
